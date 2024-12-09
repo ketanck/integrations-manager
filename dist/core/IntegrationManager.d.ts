c7b78fa64eff151a5e89695584a4e55b7d6c0a0b
ketanck <ketan@cleankoding.com>
2024-12-09 11:53:01 +0530

    dist updated

--- dist/core/IntegrationManager.d.ts
+++ dist/core/IntegrationManager.d.ts
@@ -6,30 +6,92 @@ export declare class IntegrationManager {
     private linear?;
     private clickup?;
     private jira?;
+    /**
+     * @param param0 expects LinearIntegration, ClickUpIntegration and JiraIntegration objects, all are optional
+     */
     constructor({ linear, clickup, jira }: {
         linear?: LinearIntegration;
         clickup?: ClickUpIntegration;
         jira?: JiraIntegration;
     });
+    /**
+     * get Linear auth url
+     * @param scopes expects comma(,) seperated scopes without space
+     * @returns auth url user should to redirected to
+     */
     getLinearAuthorizationUrl(scopes: string): string;
+    /**
+     * get ClickUp auth url, expects no args
+     * @returns auth url user should to redirected to
+     */
     getClickUpAuthorizationUrl(): string;
+    /**
+     * get Jira auth url
+     * @param scopes expects space seperated jira scopes
+     * @returns auth url user should to redirected to
+     */
     getJiraAuthorizationUrl(scopes: string): string;
+    /**
+     * Signin user and get access token
+     * @param code expects auth code, you get from url
+     * @returns access_token, token_type, expires_in, scope
+     */
     getLinearAccessToken(code: string): Promise<any>;
-    getCickUpAccessToken(code: string): Promise<any>;
+    /**
+     * Signin user and get access token
+     * @param code axpects auth code, you get from url
+     * @returns access_token
+     */
+    getClickUpAccessToken(code: string): Promise<string>;
+    /**
+     * Signin user and get access token
+     * @param code expects auth code, you get from url
+     * @returns access_token, expires_in and scope
+     */
     getJiraAccessToken(code: string): Promise<any>;
+    /**
+     * Fetch user info from Linear
+     * @param accessToken expects accessToken as argument
+     * @returns User model returned by linear
+     */
     linearUser(accessToken: string): Promise<User>;
+    /**
+     * Fetch user info from ClickUp
+     * @param accessToken expects accessToken as argument
+     * @returns user info returned by clickup
+     */
     clickUpUser(accessToken: string): Promise<any>;
+    /**
+     * Fetch user info from jira
+     * @param accessToken expects accessToken as argument
+     * @returns user info returned by jira
+     */
     jiraUser(accessToken: string): Promise<any>;
+    /**
+     * Create new task on Linear
+     * @param param0 expects accessToken, title and description to create task on linear
+     * @returns linear response
+     */
     createTaskOnLinear({ accessToken, title, description }: {
         accessToken: string;
         title: string;
         description: string;
     }): Promise<any>;
+    /**
+     * Create new task on ClickUp
+     * @param param0 expects accessToken, title and description to create task on clickup
+     * @returns clickup response
+     */
     createTaskOnClickUp({ accessToken, title, description }: {
         accessToken: string;
         title: string;
         description: string;
     }): Promise<any>;
+    /**
+     * Create new task on Jira
+     * @param param0 expects accessToken, title and description to create task on jira
+     * @returns jira response
+     */
     createTaskOnJira({ accessToken, summary, description }: {
         accessToken: string;
         summary: string;
--- dist/core/IntegrationManager.js
+++ dist/core/IntegrationManager.js
@@ -16,6 +16,9 @@ var Platforms;
     Platforms[Platforms["clickup"] = 1] = "clickup";
 })(Platforms || (Platforms = {}));
 class IntegrationManager {
+    /**
+     * @param param0 expects LinearIntegration, ClickUpIntegration and JiraIntegration objects, all are optional
+     */
     constructor({ linear, clickup, jira }) {
         if (linear) {
             this.linear = linear;
@@ -28,31 +31,60 @@ class IntegrationManager {
         }
     }
     // #####################   AUTHORIZATION LINK   ########################
+    /**
+     * get Linear auth url
+     * @param scopes expects comma(,) seperated scopes without space
+     * @returns auth url user should to redirected to
+     */
     getLinearAuthorizationUrl(scopes) {
         var _a, _b;
         return (_b = (_a = this.linear) === null || _a === void 0 ? void 0 : _a.authorize(scopes)) !== null && _b !== void 0 ? _b : "Linear Integration not added";
     }
+    /**
+     * get ClickUp auth url, expects no args
+     * @returns auth url user should to redirected to
+     */
     getClickUpAuthorizationUrl() {
         var _a, _b;
         return (_b = (_a = this.clickup) === null || _a === void 0 ? void 0 : _a.authorize()) !== null && _b !== void 0 ? _b : "ClickUp Integration not added";
     }
+    /**
+     * get Jira auth url
+     * @param scopes expects space seperated jira scopes
+     * @returns auth url user should to redirected to
+     */
     getJiraAuthorizationUrl(scopes) {
         var _a, _b;
         return (_b = (_a = this.jira) === null || _a === void 0 ? void 0 : _a.authorize(scopes)) !== null && _b !== void 0 ? _b : "Jira Integration not added";
     }
     // ####################   GET ACCESS TOKEN   ########################
+    /**
+     * Signin user and get access token
+     * @param code expects auth code, you get from url
+     * @returns access_token, token_type, expires_in, scope
+     */
     getLinearAccessToken(code) {
         return __awaiter(this, void 0, void 0, function* () {
             var _a;
             return yield ((_a = this.linear) === null || _a === void 0 ? void 0 : _a.getTokens(code));
         });
     }
-    getCickUpAccessToken(code) {
+    /**
+     * Signin user and get access token
+     * @param code axpects auth code, you get from url
+     * @returns access_token
+     */
+    getClickUpAccessToken(code) {
         return __awaiter(this, void 0, void 0, function* () {
-            var _a;
-            return yield ((_a = this.clickup) === null || _a === void 0 ? void 0 : _a.getTokens(code));
+            var _a, _b;
+            return (_b = yield ((_a = this.clickup) === null || _a === void 0 ? void 0 : _a.getTokens(code))) !== null && _b !== void 0 ? _b : "";
         });
     }
+    /**
+     * Signin user and get access token
+     * @param code expects auth code, you get from url
+     * @returns access_token, expires_in and scope
+     */
     getJiraAccessToken(code) {
         return __awaiter(this, void 0, void 0, function* () {
             var _a;
@@ -60,18 +92,33 @@ class IntegrationManager {
         });
     }
     // ###################   FETCH USER INFO   ###################
+    /**
+     * Fetch user info from Linear
+     * @param accessToken expects accessToken as argument
+     * @returns User model returned by linear
+     */
     linearUser(accessToken) {
         return __awaiter(this, void 0, void 0, function* () {
             var _a;
             return yield ((_a = this.linear) === null || _a === void 0 ? void 0 : _a.fetchUserInfo(accessToken));
         });
     }
+    /**
+     * Fetch user info from ClickUp
+     * @param accessToken expects accessToken as argument
+     * @returns user info returned by clickup
+     */
     clickUpUser(accessToken) {
         return __awaiter(this, void 0, void 0, function* () {
             var _a;
             return yield ((_a = this.clickup) === null || _a === void 0 ? void 0 : _a.fetchUserInfo(accessToken));
         });
     }
+    /**
+     * Fetch user info from jira
+     * @param accessToken expects accessToken as argument
+     * @returns user info returned by jira
+     */
     jiraUser(accessToken) {
         return __awaiter(this, void 0, void 0, function* () {
             var _a;
@@ -79,18 +126,33 @@ class IntegrationManager {
         });
     }
     // #####################   CREATE NEW TASK   ####################
+    /**
+     * Create new task on Linear
+     * @param param0 expects accessToken, title and description to create task on linear
+     * @returns linear response
+     */
     createTaskOnLinear(_a) {
         return __awaiter(this, arguments, void 0, function* ({ accessToken, title, description }) {
             var _b;
             return yield ((_b = this.linear) === null || _b === void 0 ? void 0 : _b.createTask({ accessToken, title, description }));
         });
     }
+    /**
+     * Create new task on ClickUp
+     * @param param0 expects accessToken, title and description to create task on clickup
+     * @returns clickup response
+     */
     createTaskOnClickUp(_a) {
         return __awaiter(this, arguments, void 0, function* ({ accessToken, title, description }) {
             var _b;
             return yield ((_b = this.clickup) === null || _b === void 0 ? void 0 : _b.createTask({ accessToken, title, description }));
         });
     }
+    /**
+     * Create new task on Jira
+     * @param param0 expects accessToken, title and description to create task on jira
+     * @returns jira response
+     */
     createTaskOnJira(_a) {
         return __awaiter(this, arguments, void 0, function* ({ accessToken, summary, description }) {
             var _b;
