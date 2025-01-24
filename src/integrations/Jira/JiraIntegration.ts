d5a8cf12099494a0a0db5dd0e1e7e39745c292a3
ketanck <ketan@cleankoding.com>
2025-01-24 16:18:19 +0530

    removed logs

--- src/integrations/Jira/JiraIntegration.ts
+++ src/integrations/Jira/JiraIntegration.ts
@@ -62,12 +62,6 @@ export class JiraIntegration implements JiraInterface {
             "Content-Type": "application/json"
         };
 
-        console.log("CODE: " + code);
-        console.log("REDIRECT: " + redirectUrl);
-        console.log("CLIENT ID: " + this.clientId);
-        console.log("CLIENT SECRET: " + this.clientSecret);
-        
-
         try {
             const res: axios.AxiosResponse = await axios.post(JiraIntegration.tokenUrl,
                 data,
